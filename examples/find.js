48ddf360f6a353fe86c7d66d30a2c47aa29055a8
Miramac <miramac@gmx.net>
2014-09-10 10:43:05 +0200

    Merge remote-tracking branch 'origin/master'
    Conflicts:
    	test/find.js

---
+++ examples/find.js
@@ -0,0 +1,23 @@
+var dirco = require('../') //used the ES5 version, use require('../lib/dirco') for ES6
+, patter = /^.+\.js$/i; //find all .js files
+
+dirco(__dirname+'/../', {depth:-1, stats:false},function(err, result) {
+  console.log(find(result,patter)); 
+});
+
+
+function find(root, patter) {
+    var item, items = []
+    , i ;
+    for(i=0; i<root.length;i++) {
+      item = root[i];
+      if(item.name.match(patter)) {
+        items.push(item.path);
+      } else {
+        if(item.children) {
+          items = items.concat(find(item.children, patter));
+        }
+      }
+    }
+    return items;
+}
