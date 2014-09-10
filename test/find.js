5da9708ef84726ca38c1efefc1a3066b60bdc06e
Miramac <miramac@gmx.net>
2014-09-10 10:40:58 +0200

    update find

--- test/find.js
+++ test/find.js
@@ -2,12 +2,27 @@ var dirco = require('../')
 , util = require('util')
 , patter = /^\.bin$/i;
 
-dirco("../node_modules", {depth:1, stats:'size'},function(err, result) {
+dirco("../node_modules", {depth:-1, stats:'size'},function(err, result) {
   var i;
  // console.log(util.inspect(result, {showHidden: false, depth: null}));
-  for(i=0; i<result.length; i++) {
-    if(patter.test(result[i].name)) {
-      console.log(util.inspect(result[i], {showHidden: false, depth: null}));
+  console.log(util.inspect(search(result, patter), {showHidden: false, depth: null}));
+  
+});
+
+function search(root, find) {
+  var i, j
+  , results = [];
+  if(!root.length) return results;
+  for (i=0; i<root.length; i++) {console.log(root[i])
+    if(root[i].children) {
+       console.log(root[i].children)
+      for(j=0; root[i].children.length; j++) {
+        results = results.concat(search(root[i].children[j], find));
+      }
+    }
+    if (find.test(root[i].name)) {
+      results.push(root[i]);
     }
   }
-});
+  return results;
+}
